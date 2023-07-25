620b3639e8023203e7ccfec105e50ccb70d22c64
Hidenori <hfu@users.noreply.github.com>
2023-07-25 22:56:22 +0900

    update

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # mosaic
 WIP
+
+## Generated data
+### FlatGeobuf - zoom-by-zoom from GSI optbv
+Source: Geospatial Information Authority of Japan ([最適化ベクトルタイル](https://github.com/gsi-cyberjapan/optimal_bvmap))
+- [4.fgb](https://smb.optgeo.org/ipfs/QmVbkTdTdzbziqWYMezfWgPAgqeC4vxyN6EcoD1ovEXcBR?filename=4.fgb)
+- [5.fgb](https://smb.optgeo.org/ipfs/QmSs5n9GYgZyCtq5FjemHX8NdVTCHiPpqaXMKHn1D9zexj?filename=5.fgb)
+- [6.fgb](https://smb.optgeo.org/ipfs/QmVijZvMp9yAKLiBXu5qzsDLgDctjfcmbQebmvp62cF6uE?filename=6.fgb)
+- [7.fgb](https://smb.optgeo.org/ipfs/QmbBu7UmYMqdBEGL8qrbXAqnTdZFbWLy8vpcUqXEvfo19h?filename=7.fgb)
+- [8.fgb](https://smb.optgeo.org/ipfs/QmRW9V7QRtBRYyDJbwwPSYdZQeeGPfzKfgascs8BxMBR1c?filename=8.fgb)
+- [9.fgb](https://smb.optgeo.org/ipfs/QmQ7M2k28Fpgv8kxi9LuXttjdNZAX82E2j4qytyHzq4jw5?filename=9.fgb)
+- 10.fgb
+- 11.fgb
+- 12.fgb
+- 13.fgb
+- 14.fgb
+- 15.fgb
+- 16.fgb
+
+#### Usage example
+You can read the file from QGIS using the URLs above by adding vector layer, source type Protocol. 
+
+![screenshot](https://user-images.githubusercontent.com/18297/255915090-d3fbfda0-2005-4f84-b8fe-9e03dba4b320.png)
+
+## See also
+- [mosaic: ベクトルタイルを再構築するための Rubyコマンド（構想）](https://qiita.com/hfu/items/7025946c38f9f9b4b86e)
---
+++ mosaic.rb
@@ -0,0 +1,47 @@
+require 'optparse'
+require 'tempfile'
+
+# デフォルトのオプション
+options = {
+  mokuroku_url: 'https://maps.gsi.go.jp/xyz/experimental_bvmap/mokuroku.csv.gz',
+  template_url: 'https://maps.gsi.go.jp/xyz/optimal_bvmap-v1/{z}/{x}/{y}.pbf',
+  minimum_zoom: 4,
+  maximum_zoom: 16,
+  gzcat: 'gzcat'
+}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: mosaic [options]"
+
+  opts.on("--mokuroku-url URL", "地理院地図Vectorタイルのmokuroku.csv.gzファイルのURLを指定") do |url|
+    options[:mokuroku_url] = url
+  end
+
+  opts.on("--template-url URL", "国土地理院最適化ベクトルタイルのテンプレートURLを指定") do |url|
+    options[:template_url] = url
+  end
+
+  opts.on("--minimum-zoom ZOOM", Integer, "出力するベクトルタイルの最小ズームレベルを指定") do |zoom|
+    options[:minimum_zoom] = zoom
+  end
+
+  opts.on("--maximum-zoom ZOOM", Integer, "出力するベクトルタイルの最大ズームレベルを指定") do |zoom|
+    options[:maximum_zoom] = zoom
+  end
+
+  opts.on("--gzcat CMD", "GZip ファイルを標準出力に展開するコマンド名を指定") do |cmd|
+    options[:gzcat] = cmd
+  end
+end.parse!
+
+mokuroku = Kernel.open("| curl --silent #{options[:mokuroku_url]} | gzcat")
+mokuroku.each do |l|
+  (z, x, y) = l.split(',')[0].split('/').map{|v| v.to_i}
+  next if z < options[:minimum_zoom] or z > options[:maximum_zoom]
+  url = options[:template_url].sub('{z}', z.to_s).sub('{x}', x.to_s).sub('{y}', y.to_s)
+  Tempfile.create('mosaic') do |f|
+    path = f.path
+    system "curl --silent -o #{path} #{url}"
+    system "tippecanoe-decode -c #{path} #{z} #{x} #{y} | tippecanoe-json-tool"
+  end
+end
